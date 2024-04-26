12f0daae6357e99f69841f697e1ad2457f740234
Seth Battis <seth@battis.net>
2024-04-25 19:52:17 -0700

    Fix project ID in billing
    close #7

--- src/billing/projects/index.ts
+++ src/billing/projects/index.ts
@@ -7,17 +7,18 @@ export async function enable({
   account,
   projectId
 }: { account?: string; projectId?: string } = {}) {
-  cli.log.debug({ projectId });
   // TODO create a new billing account interactively
   account = await accounts.selectidentifier({
     name: account,
     purpose: 'to link to the project'
   });
   if (account) {
-    projectId = await rootProjects.selectIdentifier({
-      projectId,
-      purpose: `to link to billing account ${cli.colors.value(account)}`
-    });
+    projectId =
+      projectId ||
+      (await rootProjects.selectIdentifier({
+        projectId,
+        purpose: `to link to billing account ${cli.colors.value(account)}`
+      }));
     await shell.gcloudBeta(
       `billing projects link ${projectId} --billing-account=${account}`,
       { includeProjectIdFlag: false }
