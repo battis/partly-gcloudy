a01299373308912d6f2b3898484a4182f5cb2d07
Seth Battis <seth@battis.net>
2025-02-27 09:41:07 -0500

    feat: publish on `batch.appEngineDeployAndCleanup()` if no active project

--- src/batch/appEngineDeployAndCleanUp.ts
+++ src/batch/appEngineDeployAndCleanUp.ts
@@ -1,40 +1,35 @@
-import { Shell } from '@battis/qui-cli.shell';
 import * as app from '../app.js';
 import * as core from '../core.js';
 import * as projects from '../projects.js';
-import { PreBuildCallback } from './appEnginePublish.js';
+import { appEnginePublish, PreBuildCallback } from './appEnginePublish.js';
 
 export async function appEngineDeployAndCleanup({
-  preBuild,
-  build,
-  retainVersions
-}: {
-  preBuild?: PreBuildCallback;
-  build?: string;
+  retainVersions,
+  ...options
+}: Partial<Parameters<typeof appEnginePublish>>[0] & {
   retainVersions?: number;
 } = {}) {
   if (core.ready()) {
-    const project = await projects.describe();
-    const appEngine = await app.describe();
-    if (project && appEngine && preBuild) {
-      if (!preBuild({ project, appEngine })) {
-        throw new Error('Pre-build callback failed');
+    let activeVersion: app.DeploymentConfig | undefined = undefined;
+    if (!projects.active.get()) {
+      const result = await appEnginePublish(options);
+      if (result) {
+        activeVersion = result?.deployment;
       }
+    } else {
+      activeVersion = await app.deploy();
     }
-
-    if (build) {
-      Shell.exec(build);
-    }
-
-    const activeVersion = await app.deploy();
-    if (retainVersions !== undefined && retainVersions > 0) {
-      const versions = await app.versions.list();
-      for (let i = retainVersions; i < versions.length; i++) {
-        app.versions.delete_({ version: versions[i].id });
+    if (activeVersion) {
+      if (retainVersions !== undefined && retainVersions > 0) {
+        const versions = await app.versions.list();
+        for (let i = retainVersions; i < versions.length; i++) {
+          if (versions[i].id !== activeVersion?.versions[0].id)
+            app.versions.delete_({ version: versions[i].id });
+        }
       }
-    }
 
-    return { activeVersion };
+      return { activeVersion };
+    }
   }
   return false;
 }
