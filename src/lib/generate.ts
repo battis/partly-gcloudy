3392588b35d250f5ddeaebbb678ccf5a6f410000
Seth Battis <seth@battis.net>
2025-06-17 21:37:08 -0400

    feat: update to rword@4
    Requires doing our own word length filter

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -1,29 +1,32 @@
 import crypto from 'node:crypto';
-import { rword } from 'rword';
+import { Rword } from 'rword';
+import { words as english } from 'rword-english-extended';
 
 /**
-   * Google Cloud-style project ID generator
-   *
-   * Two random words hyphenated with a 6-digit suffix, max 30 characters: owl-phlegmatic-522355
-
-   * @param length (Optional, default `30`) maximum total characters
-   * @param words (Optional, default `2`) number of words to generate
-   * @param digits (Optional, default `6`) number of digits in suffix
-   * @param separator (Optional, default `-`) separator between words
-   */
+ * Google Cloud-style project ID generator
+ *
+ * Two random words hyphenated with a 6-digit suffix, max 30 characters:
+ * owl-phlegmatic-522355
+ *
+ * @param length (Optional, default `30`) maximum total characters
+ * @param words (Optional, default `2`) number of words to generate
+ * @param digits (Optional, default `6`) number of digits in suffix
+ * @param separator (Optional, default `-`) separator between words
+ */
 export function projectId(length = 30, words = 2, digits = 6, separator = '-') {
   const word: string[] = [];
   for (let i = 0; i < words; i++) {
-    word.push(
-      rword.generate(1, {
-        length: `${(length - digits - words * separator.length) / words / 2}-${
+    const selection = english.filter(
+      (w) =>
+        w.length >= (length - digits - words * separator.length) / words / 2 &&
+        w.length <=
           length -
-          digits -
-          words * separator.length -
-          word.reduce((sum, w) => sum + w.length, 0) / (words - i)
-        }`
-      })
+            digits -
+            words * separator.length -
+            word.reduce((sum, w) => sum + w.length, 0) / (words - i)
     );
+    const rword = new Rword(selection);
+    word.push(...rword.generate());
   }
   const suffix = Math.floor(
     Math.pow(10, digits - 1) -
@@ -36,7 +39,7 @@ export function projectId(length = 30, words = 2, digits = 6, separator = '-') {
 /**
  * Randomly generated password
  *
- * @param length number of characters
+ * @param length Number of characters
  * @param encoding (Optional, default `base64`) encoding for random bytes
  */
 export function password(length = 15, encoding: BufferEncoding = 'base64') {
