e7b1fa4de9c94eb764776131aa1994a1b307c29b
Seth Battis <seth@battis.net>
2025-12-06 21:29:47 -0500

    fix!: remove deprecated iam.enableAppEngineAccess()
    BREAKING: iam.enableAppEngineAccess() was an alias to the now-deprecated
    batch.enableAppEngineSecretsAccess(). The preferred approach is to now
    either initially enable secretsAccess in batch.app.initialize() or to
    use batch.iam.enableServiceAccountSecretsAccess()

--- src/secrets/index.ts
+++ src/secrets/index.ts
@@ -1,10 +1,2 @@
-import { enableAppEngineSecretsAccess } from '../batch/enableAppEngineSecretsAccess.js';
-
 export * from './set.js';
 export * as versions from './versions/index.js';
-
-/**
- * @deprecated Use
- *   {@link enableAppEngineSecretsAccess batch.enableAppEngineSecretsAccess()}
- */
-export const enableAppEngineAccess = enableAppEngineSecretsAccess;
